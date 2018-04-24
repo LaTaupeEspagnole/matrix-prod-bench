efdfe944d4e6ed74b0bcb3c8cd48c2b05774f4f3
Sebastien Goubeau <sebastien.goubeau@epita.fr>
2018-04-24 15:46:32 +0200

    test: Progress on the command execution program
    	modified:   tests/test.c

--- tests/test.c
+++ tests/test.c
@@ -1,26 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #include "libmatrix.h"
+#include "buffer.h"
 
-void init_random(struct mat *a)
+/*
+** Gets the item at index 'index_item' in the string
+** (index are separated by ';')
+*/
+struct buffer *get_item(char *str, size_t index_item)
 {
-  size_t size = a->width * a->height;
-  for (size_t i = 0; i < size; ++i)
+  while (index_item != 0)
   {
-    a->array[i] = (float)(rand() % 10000) / 100;
+    if (*str == '\0')
+      return NULL;
+    while (*str != ';')
+    {
+      ++str;
+      if (*str == '\0')
+        return NULL;
+    }
+    ++str;
+    --index_item;
   }
+
+  struct buffer *res = init_buffer();
+  if (!res)
+    return NULL;
+
+  while (*str != '\0')
+  {
+    add_buffer(res, str, 1);
+    ++str;
+  }
+  return res;
 }
 
 int main(int argc, char *argv[])
 {
-  if (argc < 3)
+  if (argc != 2)
   {
-    printf("Usage: %s [mat_width] [mat_height]\n", argv[0]);
+    printf("Usage: %s [help|command]\n", argv[0]);
+    return 1;
+  }
+
+  if (!strcmp(argv[1], "help"))
+  {
+    printf("Help : ");
+    printf("This program takes a command as parameter and stdin\n");
+    printf("data and returns stdout data related to the command\n\n");
+    printf("List of the commands and the related function called :\n\n");
+    printf("  +---------+-----------------+------------+--------------+\n");
+    printf("  | Command | Function        | stdin      | stdout       |\n");
+    printf("  +---------+-----------------+------------+--------------+\n");
+    printf("  | dec     | dec_mat()       | [num;num;] | 1 matrix     |\n");
+    printf("  | ini     | init_mat()      | [num;num;] | 1 matrix     |\n");
+    printf("  | cpy     | copy_mat()      | 1 matrix   | 1 matrix     |\n");
+    printf("  | ieq     | is_equal()      | 2 matrices | [True|False] |\n");
+    printf("  | add     | add_mat()       | 2 matrices | 1 matrix     |\n");
+    printf("  | adi     | add_mat_ip()    | 2 matrices | 1 matrix     |\n");
+    printf("  | sub     | sub_mat()       | 2 matrices | 1 matrix     |\n");
+    printf("  | sui     | sub_mat_ip()    | 2 matrices | 1 matrix     |\n");
+    printf("  | mul     | mult_mat()      | 2 matrices | 1 matrix     |\n");
+    printf("  | mut     | mult_mat_th()   | 2 matrices | 1 matrix     |\n");
+    printf("  | mua     | mult_mat_auto() | 2 matrices | 1 matrix     |\n");
+    printf("  | div     | div_mat()       | 1 matrix   | 1 matrix     |\n");
+    printf("  | dii     | div_mat_ip()    | 1 matrix   | 1 matrix     |\n");
+    printf("  | scm     | sc_mult_mat()   | 1 matrix   | 1 matrix     |\n");
+    printf("  | tra     | trans_mat()     | 1 matrix   | 1 matrix     |\n");
+    printf("  +---------+-----------------+------------+--------------+\n");
+    printf("\n");
+    printf("Return values :\n");
+    printf(" 0 -> Everything whent well.\n");
+    printf(" 1 -> Not a valid command.\n");
+    printf(" 2 -> Memory allocation problems.\n");
+    printf(" 3 -> Invalid input (parsing error).\n");
+    printf("\n");
     return 0;
   }
 
+  struct buffer *buffer = init_buffer();
+  char c = 0;
+
+  if (!buffer)
+  {
+    perror("init_buffer: Unable to create buffer");
+    return 2;
+  }
+
+  if (!strcmp(argv[1], "dec"))
+  {
+    c = getchar();
+    while (c != EOF && c != '\n')
+    {
+      add_buffer(buffer, &c, 1);
+      c = getchar();
+    }
+    struct buffer *item1 = get_item(buffer->buffer, 0);
+    struct buffer *item2 = get_item(buffer->buffer, 1);
+    if (!item1 || !item2)
+    {
+      fprintf(stderr, "Invalide input format.");
+      return 3;
+    }
+    size_t num1 = atoi(item1->buffer);
+    size_t num2 = atoi(item2->buffer);
+    struct mat *a = dec_mat(num1, num2);
+    char *res = serialize_mat(a);
+    printf("%s", res);
+    free_buffer(item1);
+    free_buffer(item2);
+    free_mat(a);
+    free(res);
+  }
+  free_buffer(buffer);
+/*
   srand(time(NULL));
   size_t mat_width = atoi(argv[1]);
   size_t mat_height = atoi(argv[2]);
@@ -41,6 +137,6 @@ int main(int argc, char *argv[])
   free_mat(a);
   free_mat(b);
   free(res);
-
+*/
   return 0;
 }
